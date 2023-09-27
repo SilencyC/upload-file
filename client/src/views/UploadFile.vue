bf6c075a2bafa106bb60f5f792e04fc146999a68
sileo <634797258@qq.com>
2023-09-27 16:42:28 +0800

    feat: handle concurrent retry and error

--- client/src/views/UploadFile.vue
+++ client/src/views/UploadFile.vue
@@ -33,6 +33,12 @@ type IContainer = {
 
 // 定义切片大小
 const CHUNK_SIZE = 10 * 1024;
+const STATUS = {
+  WAIT: 'wait',
+  ERROR: 'error',
+  UPLOADING: 'uploading',
+  DONE: 'done',
+};
 
 const container = reactive<IContainer>({
   file: null,
@@ -116,7 +122,7 @@ async function uploadChunks(data: TData, uploadedList: string[] = []) {
       formData.append('fileHash', fileHash);
       formData.append('filename', container.file?.name || '');
 
-      return { formData, index };
+      return { formData, index, status: STATUS.WAIT };
     });
   //   .map(({ formData, index }) =>
   //     request({
@@ -200,16 +206,19 @@ function sendRequest(
   forms: {
     formData: FormData;
     index: number;
+    status: string;
   }[],
   data: TItemData[],
   max = 4, // 最大并发请求数，默认为4
 ) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     const len = forms.length; // 获取forms数组的长度
     // 初始化一个索引变量，用于跟踪当前处理的FormData
     let idx = 0;
     // 初始化一个计数器，用于跟踪已完成的请求数量
     let counter = 0;
+    const retryArr: number[] = [];
+
     const start = async () => {
       // 有请求，有通道
       // 当索引小于FormData数组的长度且最大并发数大于0时，继续执行
@@ -217,29 +226,66 @@ function sendRequest(
         // 减少可用的并发数，表示占用一个通道
         max--; // 占用通道
         console.log(idx, 'start');
-        const form = forms[idx].formData;
-        const index = forms[idx].index;
+        // const form = forms[idx].formData;
+        // const index = forms[idx].index;
 
-        idx++; // 增加索引以处理下一个表单
+        // idx++; // 增加索引以处理下一个表单
+
+        // 任务不能仅仅累加获取，而是要根据状态
+        // wait 和 error的可以发出请求 方便重试
+        // 根据状态找到下一个要处理的表单的索引
+        const i = forms.findIndex((v) => v.status == STATUS.WAIT || v.status == STATUS.ERROR);
+
+        // 更新表单状态为UPLOADING，表示正在上传
+        forms[i].status = STATUS.UPLOADING;
+
+        const form = forms[i].formData;
+        const index = forms[i].index;
+
+        if (typeof retryArr[index] === 'number') {
+          console.log(index, '开始重试');
+        }
 
         request({
           url: 'http://localhost:3000/request',
           data: form,
           onprogress: createProgressHandler(data[index]),
           requestList: requestXhrList,
-        }).then((res) => {
-          // 增加可用的并发数，表示释放一个通道
-          max++; // 释放通道
-          counter++;
+        })
+          .then((res) => {
+            forms[i].status = STATUS.DONE;
+            // 增加可用的并发数，表示释放一个通道
+            max++; // 释放通道
+            counter++;
 
-          if (counter === len) {
-            // 如果已完成的请求数量等于数组的长度，解析Promise
-            resolve(res);
-          } else {
-            // 否则继续处理下一个表单
+            if (counter === len) {
+              // 如果已完成的请求数量等于数组的长度，解析Promise
+              resolve(res);
+            } else {
+              // 否则继续处理下一个表单
+              start();
+            }
+          })
+          .catch(() => {
+            forms[i].status = STATUS.ERROR;
+            if (typeof retryArr[index] !== 'number') {
+              retryArr[index] = 0;
+            }
+
+            //次数累加
+            retryArr[index]++;
+            if (retryArr[index] >= 2) {
+              // 一个请求报错三次
+              return reject();
+            }
+            console.log(index, retryArr[index], '报错次数');
+            // 3次报错以内的 重启
+            data[index].percentage = 0; // 报错的进度条重置
+            max++; // 释放当前占用的通道，但是counter不累加
+            idx++; // 增加索引以处理下一个表单
+
             start();
-          }
-        });
+          });
       }
     };
     // 调用start函数，开始处理请求
